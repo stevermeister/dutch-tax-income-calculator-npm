d23040c57503adc48c9181c49e62b899834d8f21
Stepan Suvorov <stevermeister@gmail.com>
2020-01-28 14:38:23 +0100

    cleanup + updates

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 const constants = require("./data.json"); // Get JSON containing calculation constants
 
-// income: 36000,
-let grossYear = 36000;
-let year = 2020;
-let older = false;
-let hours = constants.defaultWorkingHours;
-let ruling = {
-  checked: false,
-  choice: "normal"
-};
-
 // For calculation instructions:
 // https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/themaoverstijgend/brochures_en_publicaties/rekenvoorschriften-voor-de-geautomatiseerde-loonadministratie-januari-2017
 
@@ -31,10 +21,11 @@ function getNetYear(grossYear, ruling, older, year = 2020) {
   }
 
   let incomeTax = ~~(
-    + getSocialCredit(year, older) * getGeneralCredit(year, taxableYear, older)
-    + getSocialCredit(year, older) * getLabourCredit(year, taxableYear, older) 
-    - getPayrollTax(year, taxableYear) 
-    - getSocialTax(year, taxableYear, older));
+    + getGeneralCredit(year, taxableYear, getSocialCredit(year, older))
+    + getLabourCredit(year, taxableYear, getSocialCredit(year, older))
+    - getPayrollTax(year, taxableYear)
+    - getSocialTax(year, taxableYear, older)
+  );
   incomeTax = incomeTax < 0 ? incomeTax : 0;
   return ~~taxableYear + incomeTax + ~~taxFreeYear;
 }
@@ -44,7 +35,9 @@ function getNetMonth(grossYear, ruling, older = false, year = 2020) {
 }
 
 function getNetWeek(grossYear, ruling, older = false, year = 2020) {
-  return ~~(getNetYear(grossYear, ruling, older, year) / constants.workingWeeks);
+  return ~~(
+    getNetYear(grossYear, ruling, older, year) / constants.workingWeeks
+  );
 }
 
 function getNetDay(grossYear, ruling, older = false, year = 2020) {
@@ -52,23 +45,45 @@ function getNetDay(grossYear, ruling, older = false, year = 2020) {
 }
 
 function getNetHour(grossYear, ruling, older = false, year = 2020, hours) {
-  return ~~(getNetYear(grossYear, ruling, older, year) / (constants.workingWeeks * hours));
+  return ~~(
+    getNetYear(grossYear, ruling, older, year) /
+    (constants.workingWeeks * hours)
+  );
 }
 
-// 30% Ruling (30%-regeling)
-// https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/internationaal/werken_wonen/tijdelijk_in_een_ander_land_werken/u_komt_in_nederland_werken/30_procent_regeling/voorwaarden_30_procent_regeling/u_hebt_een_specifieke_deskundigheid
+/**
+ * 30% Ruling (30%-regeling)
+ * https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/internationaal/werken_wonen/tijdelijk_in_een_ander_land_werken/u_komt_in_nederland_werken/30_procent_regeling/voorwaarden_30_procent_regeling/u-hebt-een-specifieke-deskundigheid
+ * 
+ * @param {string} year Year to retrieve information from
+ * @param {string} ruling Choice between scientific research workers, young professionals with Master's degree or others cases
+ * @returns {number} The 30% Ruling minimum income
+ */
 function getRulingIncome(year, ruling) {
   return constants.rulingThreshold[year][ruling];
 }
 
-// Payroll Tax Rates (Loonbelasting)
-// https://www.belastingdienst.nl/bibliotheek/handboeken/html/boeken/HL/stappenplan-stap_7_loonbelasting_premie_volksverzekeringen.html
+/**
+ * Payroll Tax Rates (Loonbelasting)
+ * https://www.belastingdienst.nl/bibliotheek/handboeken/html/boeken/HL/stappenplan-stap_7_loonbelasting_premie_volksverzekeringen.html
+ * 
+ * @param {string} year Year to retrieve information from
+ * @param {number} salary Taxable wage that will be used for calculation
+ * @returns {number} The Payroll Tax Rates after calculating proper bracket amount
+ */
 function getPayrollTax(year, salary) {
   return getRates(constants.payrollTax[year], salary, "rate");
 }
 
-// Social Security Contribution (Volksverzekeringen - AOW, Anw, Wlz)
-// https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/werk_en_inkomen/sociale_verzekeringen/premies_volks_en_werknemersverzekeringen/volksverzekeringen/volksverzekeringen?projectid=98f8c360-e92a-4fe2-aea6-27e9087ce4a1&projectid=98f8c360-e92a-4fe2-aea6-27e9087ce4a1
+/**
+ * Social Security Contribution (Volksverzekeringen - AOW, Anw, Wlz)
+ * https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/werk_en_inkomen/sociale_verzekeringen/premies_volks_en_werknemersverzekeringen/volksverzekeringen/volksverzekeringen
+ * 
+ * @param {string} year Year to retrieve information from
+ * @param {number} salary Taxable wage that will be used for calculation
+ * @param {string} [older] Whether is after retirement age or not
+ * @returns {number} The Social Security Contribution after calculating proper bracket amount
+ */
 function getSocialTax(year, salary, older) {
   return getRates(
     constants.socialPercent[year],
@@ -77,20 +92,41 @@ function getSocialTax(year, salary, older) {
   );
 }
 
-// General Tax Credit (Algemene Heffingskorting)
-// https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/inkomstenbelasting/heffingskortingen_boxen_tarieven/heffingskortingen/algemene_heffingskorting/
-function getGeneralCredit(year, salary) {
-  return getRates(constants.generalCredit[year], salary, "rate");
+/**
+ * General Tax Credit (Algemene Heffingskorting)
+ * https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/inkomstenbelasting/heffingskortingen_boxen_tarieven/heffingskortingen/algemene_heffingskorting/
+ * 
+ * @param {string} year Year to retrieve information from
+ * @param {number} salary Taxable wage that will be used for calculation
+ * @param {number} [multiplier] Scalar value to multiple against final result
+ * @returns {number} The General Tax Credit after calculating proper bracket amount
+ */
+function getGeneralCredit(year, salary, multiplier = 1) {
+  return getRates(constants.generalCredit[year], salary, "rate", multiplier);
 }
 
-// Labour Tax Credit (Arbeidskorting)
-// https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/inkomstenbelasting/heffingskortingen_boxen_tarieven/heffingskortingen/arbeidskorting/
-function getLabourCredit(year, salary) {
-  return getRates(constants.labourCredit[year], salary, "rate");
+/**
+ * Labour Tax Credit (Arbeidskorting)
+ * https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/inkomstenbelasting/heffingskortingen_boxen_tarieven/heffingskortingen/arbeidskorting/
+ * 
+ * @param {string} year Year to retrieve information from
+ * @param {number} salary Taxable wage that will be used for calculation
+ * @param {number} [multiplier] Scalar value to multiple against final result
+ * @returns {number} The Labout Tax Credit after calculating proper bracket amount
+ */
+function getLabourCredit(year, salary, multiplier = 1) {
+  return getRates(constants.labourCredit[year], salary, "rate", multiplier);
 }
 
-// Social Security Contribution (Volksverzekeringen) Component of Tax Credit
-// https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/internationaal/fiscale_regelingen/sociale_zekerheid_bij_grensoverschrijdend_werken_en_ondernemen/hoe_wordt_de_premie_berekend/berekening_premie_volksverzekeringen_heffingskorting_deel_van_het_jaar_premieplichtig/heffingskortingen/
+/**
+ * Social Security Contribution (Volksverzekeringen) Component of Tax Credit
+ * https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/werk_en_inkomen/sociale_verzekeringen/premies_volks_en_werknemersverzekeringen/volksverzekeringen/hoeveel_moet_u_betalen
+ * 
+ * @param {string} year Year to retrieve information from
+ * @param {boolean} older Whether is after retirement age or not
+ * @param {boolean} socialSecurity Whether social security will be considered or not
+ * @returns {number} Social Security contribution percentage to apply to wage credit
+ */
 function getSocialCredit(year, older) {
   /*
    * JSON properties for socialPercent object
@@ -106,27 +142,41 @@ function getSocialCredit(year, older) {
   return percentage;
 }
 
-// https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/themaoverstijgend/brochures_en_publicaties/handboek-loonheffingen-2017
-function getRates(brackets, salary, type) {
+/**
+ * Get right amount based on the rate brackets passed
+ * https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/themaoverstijgend/brochures_en_publicaties/nieuwsbrief-loonheffingen-2020
+ *
+ * @param {object[]} brackets Rate brackets to extract information from
+ * @param {number} salary Taxable wage that will be used for calculation
+ * @param {string} kind Property name to be extracted from bracket
+ * @param {number} [multiplier] Scalar value to multiple against final result
+ * @returns {number} Accumulated tax/credit amount to be used to calculate the net income
+ */
+function getRates(brackets, salary, kind, multiplier = 1) {
   let amount = 0,
     tax,
     delta,
-    percent;
+    isPercent;
 
   brackets.some((bracket, index) => {
     delta = bracket.max ? bracket.max - bracket.min : Infinity; // Consider infinity when no upper bound
-    tax = type && bracket[type] ? bracket[type] : bracket["rate"];
-    percent = tax != 0 && tax > -1 && tax < 1; // Check if rate is percentage or fixed
+    tax =
+      Math.round(
+        multiplier *
+          (kind && bracket[kind] ? bracket[kind] : bracket["rate"]) *
+          100000
+      ) / 100000;
+    isPercent = tax != 0 && tax > -1 && tax < 1; // Check if rate is percentage or fixed
     if (salary <= delta) {
-      if (percent) {
+      if (isPercent) {
         amount += Math.trunc(salary * 100 * tax) / 100; // Round down at 2 decimal places
       } else {
         amount = tax;
       }
-      //console.log(index, salary, delta, tax, percent, amount);
+      //console.log(index, salary, delta, tax, isPercent, amount);
       return true; // Break loop when reach last bracket
     } else {
-      if (percent) {
+      if (isPercent) {
         amount += delta * tax;
       } else {
         amount = tax;
@@ -136,7 +186,6 @@ function getRates(brackets, salary, type) {
   });
   return amount;
 }
-
 
 exports.getNetYear = getNetYear;
 exports.getNetMonth = getNetMonth;
@@ -151,5 +200,15 @@ exports.getSocialCredit = getSocialCredit;
 exports.getRates = getRates;
 
 
+
+
+// QUICK TEST
+let grossYear = 36000;
+let year = 2020;
+let older = false;
+let ruling = {
+  checked: false,
+  choice: "normal"
+};
 
 console.log(getNetMonth(grossYear, ruling, older, year));
